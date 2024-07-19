6ff2a110bf642887e3fde20ad7a503fe3446b644
민수 조 <msjo@kyobobook.com>
2024-07-19 10:59:21 +0900

    restore

--- 00.helloworld/digraph.g
+++
@@ -1,6 +0,0 @@
-digraph G {
-  rankdir = "RL";
-  node [shape = rect, fontname = "sans-serif"];
-  "local_file.abc" [label="local_file.abc"];
-  "local_file.def" [label="local_file.def"];
-}
--- 00.helloworld/main.tf
+++ 00.helloworld/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = "> 1.3.0" # 물결표시는 가장 자리수가 낮은 부분에 대해서라는 뜻. 따라서 ~> 1.3.0은 1.3.x는 허용한다는 뜻
 
-#   cloud {
-#     hostname = "app.terraform.io"
-#     organization = "msjo66-helloworld"
-#     workspaces {
-#       name = 
-#     }
-#   }
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "msjo66-helloworld"
+    workspaces {
+      name = "terraformStudy"
+    }
+  }
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
